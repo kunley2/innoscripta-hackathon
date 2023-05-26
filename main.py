8a6890644b3eb172d22f9b28a8babb2e4d6c2e37
kunley2 <adewoleadekunle23@yahoo.com>
2023-05-26 15:27:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from Web import create_app
 from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
 import uvicorn
-from Web.manel import lang_model
+from Web.manel import lang_model,langchain_serp
 from typing import Optional
 import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+google_cse_id = os.getenv('GOOGLE_CSE_ID')
+google_api_key = os.getenv('GOOGLE_API_KEY')
+openai_api_key = os.getenv("OPENAI_API_KEY")
+os.environ["SERPAPI_API_KEY"] = os.getenv('SERP_API_KEY')
 
 api = FastAPI()
 flask_app = create_app()
@@ -17,8 +26,12 @@ api_route = '/api/v1'
 
 @api.post(f'{api_route}/company')
 async def company(company:str,country:str,url:Optional[str]=None):
-    # print(url)
-    result = lang_model(company=company.replace(' ','-'),country=country)
+    try:
+        result = langchain_serp(company=company,country=country,
+                            openai_key=openai_api_key)
+    except:
+        result = lang_model(company=company,country=country,
+                            openai_key=openai_api_key,google_api_key=google_api_key,google_cse_id=google_cse_id)
     return {'result':result}
 
 api.mount('/',WSGIMiddleware(flask_app))
